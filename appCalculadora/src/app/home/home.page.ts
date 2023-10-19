e6139f926a5c7d802b4bf3c1b54a4c24fe10c35f
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-10-19 09:38:05 -0300

    att calculadora

--- appCalculadora/src/app/home/home.page.ts
+++ appCalculadora/src/app/home/home.page.ts
@@ -9,7 +9,50 @@ import { Component } from '@angular/core';
 
 export class HomePage {
   
-  visor : number = 0;
+  visor : string = '0';
+  operacao! : number;
+  valor1! : number;
+  valor2! : number;
+
   constructor() {}
+
+  adicionarNumero(valor : string){
+    if((this.visor === '0') && (this.visor.length === 1))
+      this.visor = valor;
+    else
+      this.visor += valor;
+  }
+
+  adicionarOperacao(valor : number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.zerar();
+  }
+
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0 : {
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      }
+      case 1 : {
+        this.visor = "" + (this.valor1 - this.valor2);
+        break;
+      }
+      case 2 : {
+        this.visor = "" + (this.valor1 * this.valor2);
+        break;
+      }
+      case 3 : {
+        this.visor = "" + (this.valor1 / this.valor2);
+        break;
+      }
+    }
+  }
+
+  zerar(){
+    this.visor = '0';
+  }
 
 }
