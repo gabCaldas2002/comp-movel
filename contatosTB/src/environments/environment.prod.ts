2a92f2826611af07db5a3f8b932df39dc23450fd
luaragiovanna <luaragiovanna3@gmail.com>
2023-11-09 09:00:17 -0300

    Upload do appContatos

---
+++ contatosTB/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyD8uiQ0DXTj2OpjU7sP7xJJVB5h6bGPeDc",
+  authDomain: "appcontatos-88cee.firebaseapp.com",
+  projectId: "appcontatos-88cee",
+  storageBucket: "appcontatos-88cee.appspot.com",
+  messagingSenderId: "443040296770",
+  appId: "1:443040296770:web:489b5d81f54a4507f122db"
+  }
+};
