7a50ba04b6cdbef4d56f4516645ae598a0bc41a8
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-12-07 09:44:28 -0300

    criação da autenticação

---
+++ contatosTB/src/app/model/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NgZone } from '@angular/core';
+import { FirebaseService } from './firebase.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  usuarioDados: any;
+
+  constructor(private firebase: FirebaseService, private router: Router, private ngZone: NgZone, private fireAuth: AngularFireAuth) { 
+    this.fireAuth.authState.subscribe(user => {
+      if(user){
+        this.usuarioDados = user;
+        localStorage.setItem('user', JSON.stringify(this.usuarioDados));
+      }else{
+        localStorage.setItem('user', 'null');
+      }
+    });
+  }
+
+  //login com email e senha
+  public signIn(email: string, password: string){}
+  public signUp(email: string, password: string){}
+  public recoverPassword(email: string){}
+
+  //métodos gerais 
+  public signOut(){}
+  public getUserLogged(){}
+  public isLogged(): boolean{
+    return false
+  }
+
+}
