86caed6c421084def1412bc72a68e7b1dbbc637d
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-12-07 09:44:10 -0300

    criação da página cadastrar

--- contatosTB/src/app/view/usuarios/signup/signup.page.ts
+++ contatosTB/src/app/view/usuarios/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  formCadastrar: FormGroup;
 
-  constructor() { }
+
+  constructor(private alert: AlertService, private router: Router, private formBuilder: FormBuilder) { 
+    this.formCadastrar = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl(''),
+      confSenha: new FormControl('')
+    });
+  }
 
   ngOnInit() {
+    this.formCadastrar = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confSenha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  get errorControl(){
+    return  this.formCadastrar.controls;
+  }
+
+  submitForm() : boolean{
+    if(!this.formCadastrar.valid){
+      this.alert.presentAlert('Erro', 'Erro ao preencher!')
+      return false
+    }else{
+      this.cadastrar()
+      return true;
+    }
+  }
+
+  private cadastrar(){
+    this.alert.presentAlert('Olá', 'Seja bem-vindo')
+    this.router.navigate(["/home"])
   }
 
 }
