34930592026cca115bd1f12a3561ef1658d68882
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2024-02-15 09:50:30 -0300

    tela de busca vazia

--- AppFood/src/app/components/components.module.ts
+++ AppFood/src/app/components/components.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { IonicModule } from '@ionic/angular';
 import { LoadingRetaurantComponent } from './loading-retaurant/loading-retaurant.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantComponent, LoadingRetaurantComponent],
+  declarations: [RestaurantComponent, LoadingRetaurantComponent, EmptyScreenComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [RestaurantComponent, LoadingRetaurantComponent]
+  exports: [RestaurantComponent, LoadingRetaurantComponent, EmptyScreenComponent]
 })
 export class ComponentsModule { }
