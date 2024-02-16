86aaf71ae749e968130efe1a32bb33a6fecf17a1
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2024-02-16 10:17:35 -0300

    criação da page itens

--- AppFood/src/app/pages/tabs/tabs-routing.module.ts
+++ AppFood/src/app/pages/tabs/tabs-routing.module.ts
@@ -31,6 +31,10 @@ const routes: Routes = [
       },
     ]
   },
+  {
+    path: 'itens',
+    loadChildren: () => import('./itens/itens.module').then( m => m.ItensPageModule)
+  },
 ];
 
 @NgModule({
