24c2866e028de20c447d8817f6cf7d918bdfb651
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2024-02-22 09:44:56 -0300

    itens page modificaçõe para o carrinho

--- AppFood/src/app/pages/tabs/itens/itens.page.ts
+++ AppFood/src/app/pages/tabs/itens/itens.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences'
 
 @Component({
   selector: 'app-itens',
@@ -109,11 +110,13 @@ export class ItensPage implements OnInit {
     },
   ];
   data: any= {};
+  cartData: any= {};
+  storeData: any = {};
   itens: any[] = [];
   id: any;
   veg : boolean = false
 
-  constructor(private route: ActivatedRoute, private navCtrl: NavController) { }
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private router: Router) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap=>{
@@ -130,13 +133,103 @@ export class ItensPage implements OnInit {
     this.data = {};
     let data : any = this.restaurants.filter(x => x.uid === this.id);
     this.data = data[0];
-    this.itens = this.allItems;
+    this.itens = this.allItems.filter(x => x.uid === this.id);
+    this.categories = this.categories.filter(x => x.uid === this.id);
+    this.cartData = {};
+    this.storeData = {};
+    let cart: any = this.getCart();
+    if(cart?.value){
+      this.storeData = JSON.parse(cart.value);
+      if(this.id == this.storeData.restaurant.uid && this.allItems.length > 0){
+        this.allItems.forEach((element: any) => {
+          this.storeData.item.forEach(ele => {
+            if(element.id != ele.id) return;
+              element.quantity = ele.quantity;
+          });
+        });
+      }
+    }
+    this.cartData.totalItem = this.storeData.totalItem;
+    this.cartData.totalPrice = this.storeData.totalPrice;
+  }
+
+  quantityPlus(item, index){
+    try{
+      if(!this.itens[index].quantity || this.itens[index].quantity == 0){
+        this.itens[index].quantity = 1;
+        this.calculate();
+      }else{
+        this.itens[index].quantity += 1;
+        this.calculate();
+      }
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  quantityMinus(item, index){
+    if(this.itens[index].quantity !== 0){
+      this.itens[index].quantity -= 1;
+    }else{
+      this.itens[index].quantity = 0;
+    }
+    this.calculate();
+  }
+
+  calculate(){
+    this.cartData.itens = [];
+    let item = this.itens.filter(x => x.quantity > 0);
+    this.cartData.itens = item;
+    console.log(this.cartData.itens);
+    this.cartData.totalPrice = 0;
+    this.cartData.totalItem = 0;
+    item.forEach(element => {
+      this.cartData.totalItem += element.quantity;
+      this.cartData.totalPrice += (parseFloat(element.price)) * (parseFloat(element.quantity));
+    });
+    this.cartData.totalPrice = parseFloat(this.cartData.totalPrice).toFixed(2);
+    if(this.cartData.totalItem == 0){
+      this.cartData.totalPrice = 0;
+      this.cartData.totalItem = 0;
+    }
+  }
+
+  async saveToCart(){
+    try{
+      this.cartData.restaurant = {}
+      this.cartData.restaurant = this.data;
+      await Preferences.set({
+        key: 'cart',
+        value: JSON.stringify(this.cartData)
+      })
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  async viewCart(){
+    if(this.cartData.itens && this.cartData.itens.length > 0){
+      await this.saveToCart();
+      this.router.navigate([this.router.url + '/cart']);
+    }
+  }
+
+  getCart(){
+    return Preferences.get({key: 'cart'});
   }
 
   getCuisine(cuisine){
     return cuisine.join(', ');
   }
 
-  vegOnly(event){}
+  vegOnly(event){
+    this.itens = [];
+    if(event.detail.checked == true){
+      this.itens = this.allItems.filter(x => x.veg == true)
+    }else{
+      this.itens = this.allItems;
+    }
+  }
 
 }
